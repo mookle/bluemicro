75f453d72164f0b820d42bc0c50f5920fc614496
mookle <mwaddilove@gmail.com>
2022-04-20 12:29:29 +0100

    Custom keymap (no macros)

--- lily58-default-left-nice_nano/keymap.cpp
+++ lily58-default-left-nice_nano/keymap.cpp
@@ -2,51 +2,77 @@
 
 #if KEYBOARD_SIDE == LEFT
 
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {KEYMAP(
-        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_SPC,
-                          LAYER_3, LAYER_3, LAYER_1, KC_SPC
-    )};
+// COLEMAK Mod-DH
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {KEYMAP(
+//+---------|---------|---------|---------|---------|---------+
+    KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,
+//|---------|---------|---------|---------|---------|---------|
+    KC_TAB,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,
+//|---------|---------|---------|---------|---------|---------|
+    KC_BSPC,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,
+//|---------|---------|---------|---------|---------|---------|---------+
+   KC_LSHIFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,    KC_LBRC,
+//+---------|---------|---------|---------|---------|---------|---------|
+                                  KC_LALT,  KC_LGUI,  LAYER_2,  KC_SPC
+//                              +---------|---------|---------|---------+
+)};
 
 void setupKeymap() {
-
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,  \
-  KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,   KC_5,  \
-  KC_DEL  ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,    \
-  KC_LSFT ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 , KC_SPC,   \
-                            LAYER_3,      LAYER_3, LAYER_1, KC_SPC \
+    // QWERTY
+    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//+---------|---------|---------|---------|---------|---------+
+    _______,  _______,  _______,  _______,  _______,  _______, \
+//|---------|---------|---------|---------|---------|---------|
+    _______,  _______,  _______,   KC_E,     KC_R,     KC_T, \
+//|---------|---------|---------|---------|---------|---------|
+   KC_LSHIFT, _______,   KC_S,     KC_D,     KC_F,    _______, \
+//|---------|---------|---------|---------|---------|---------|---------+
+   KC_LCTRL,  _______,  _______,  _______,   KC_V,     KC_B,    _______, \
+//+---------|---------|---------|---------|---------|---------|---------|
+                                  _______,  _______,  _______,  _______ \
+//                              +---------|---------|---------|---------+
 );
-
-    uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-        KC_TILD ,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,  \
-  KC_TILD ,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,  \
-  KC_DEL  ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,    \
-  KC_LSFT ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11, KC_SPC , \
-    LAYER_3,LAYER_3, LAYER_1, KC_SPC \
+    // Lower
+    uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//+---------|---------|---------|---------|---------|---------+
+    KC_GRV,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+                                  _______,  _______,  _______,  _______ \
+//                              +---------|---------|---------|---------+
 );
-
-    uint32_t layer3[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-        KC_GRAVE,KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,  \
-  KC_GRAVE,KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,  \
- _______, _______,_______,_______, _______, _______,    \
-  KC_LSFT,_______,_______,_______, _______, _______, _______, \
-   LAYER_3, LAYER_3, LAYER_1,KC_SPC \
+    // Raise
+    uint32_t layer3[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//+---------|---------|---------|---------|---------|---------+
+    KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6, \
+//|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|
+    KC_LCBR,  KC_LBRC,  KC_LPRN,  KC_RPRN,  KC_RBRC,  KC_RCBR, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  KC_EXLM,   KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+                                  _______,  _______,  LAYER_4,  KC_MINS \
+//                              +---------|---------|---------|---------+
 );
-
-    uint32_t layer4[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-        KC_GRAVE,KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,  \
-  KC_CALC,KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_VOLU,  \
- PRINT_INFO, _______,_______,_______, _______, KC_VOLD,    \
-  KC_LSFT,_______,_______,_______, _______, _______, _______, \
-   LAYER_3, LAYER_3, LAYER_1,KC_SPC \
+    // Super
+    uint32_t layer4[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//+---------|---------|---------|---------|---------|---------+
+    XXXXXXX,TG(LAYER_1),XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+                                  _______,  _______,  _______,  _______ \
+//                              +---------|---------|---------|---------+
 );
 
     /*
--- lily58-default-right-nice_nano/keymap.cpp
+++ lily58-default-right-nice_nano/keymap.cpp
@@ -2,51 +2,79 @@
 
 #if KEYBOARD_SIDE == RIGHT
 
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-   {KEYMAP(
-        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_DEL,  \
-        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPACE, 
-        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,
-     KC_SPC,   KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,   KC_ENT,
-        KC_SPC,  LAYER_2, LAYER_4, LAYER_3
-    )};
+// COLEMAK Mod-DH
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {KEYMAP(
+//          +---------|---------|---------|---------|---------|---------+
+               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_DEL,
+//          |---------|---------|---------|---------|---------|---------|
+               KC_J,     KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_QUOT,
+//          |---------|---------|---------|---------|---------|---------|
+               KC_M,     KC_N,     KC_E,     KC_I,     KC_O ,   KC_ENT,
+//+---------|---------|---------|---------|---------|---------|---------|
+    KC_RBRC,   KC_K,     KC_H,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSHIFT,
+//|---------|---------|---------|---------|---------|---------|---------+
+    KC_SPC,   LAYER_3, KC_RCTRL,  XXXXXXX \
+//+---------|---------|---------|---------+
+)};
 
 void setupKeymap() {
-
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-         KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_DEL,  \
-    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_DEL,  \
-    KC_F6,   KC_MINS, KC_EQL,  KC_LBRC,  KC_RBRC,   KC_BSLS,   \
- KC_SPC,    KC_F12,  KC_NUHS, KC_NUBS, _______,  _______,   _______,  \
-    KC_SPC,   LAYER_2, LAYER_4, LAYER_3\
+    // QWERTY
+    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//          +---------|---------|---------|---------|---------|---------+
+              _______,  _______,  _______,  _______,  _______,  KC_BSPC, \
+//          |---------|---------|---------|---------|---------|---------|
+               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    _______, \
+//          |---------|---------|---------|---------|---------|---------|
+               KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  _______, \
+//+---------|---------|---------|---------|---------|---------|---------|
+    _______,   KC_N,     KC_M,    _______,  _______,  _______,  KC_RALT, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  _______,  _______,  _______ \
+//+---------|---------|---------|---------+
+);
+    // Lower
+    uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//          +---------|---------|---------|---------|---------|---------+
+               KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  KC_LEFT,  KC_DOWN,   KC_UP,  KC_RIGHT,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_PIPE,  KC_BSLS,  _______, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    KC_UNDS,  LAYER_4,  _______,  _______ \
+//+---------|---------|---------|---------+
 );
 
-    uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-         KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_DEL,  \
-  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,   \
-KC_SPC,  KC_F12,  KC_NUTL, KC_NUPI,_______, _______, _______,  \
-  KC_SPC,  LAYER_2, LAYER_4, LAYER_3 \
+    // Raise
+    uint32_t layer3[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//          +---------|---------|---------|---------|---------|---------+
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_TILD, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  _______,  _______,  _______ \
+//+---------|---------|---------|---------+
 );
 
-    uint32_t layer3[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-         KC_6,    KC_7,    KC_8,    KC_9,     KC_0,      KC_DEL,  \
-  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_INS,  \
-  _______,  _______,  _______,  _______,  _______,  _______,   \
- KC_SPC,   _______,  _______,  _______,  _______,  _______,  _______,  \
-  KC_SPC,   LAYER_2,  LAYER_4, LAYER_3 \
-);
-
-    uint32_t layer4[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-         KC_VOLU,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_CALC,  \
-  KC_VOLU,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_CALC,  \
-  KC_VOLD,  _______,  _______,  KC_MS_BTN1,  KC_MS_UP,  KC_MS_BTN2,   \
-  KC_SPC,  PRINT_INFO,  _______,  _______,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT,  \
-  KC_SPC,   LAYER_2,  LAYER_4 , LAYER_3\
+    // Super
+    uint32_t layer4[MATRIX_ROWS][MATRIX_COLS] = KEYMAP( \
+//          +---------|---------|---------|---------|---------|---------+
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//          |---------|---------|---------|---------|---------|---------|
+              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//+---------|---------|---------|---------|---------|---------|---------|
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
+//|---------|---------|---------|---------|---------|---------|---------+
+    _______,  _______,  _______,  _______ \
+//+---------|---------|---------|---------+
 );
 
     /*
